b89e610fd2121e9eb4cae6c23461616f8b3f11a1
vardhan <vardhan@DESKTOP-5Q99B9H.mshome.net>
2023-02-22 17:26:37 +0530

    Prepared Statements Added

--- practice/src/practice/JDBCDemo.java
+++ practice/src/practice/JDBCDemo.java
@@ -2,6 +2,7 @@ package practice;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -10,17 +11,21 @@ public class JDBCDemo {
 		Connection conn = null;
 		Statement stmt = null;
 		try {
+			String sql = "SELECT * FROM mytable WHERE id = ?";
+			
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "root", "root");
-			stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM mytable");
+			//stmt = conn.createStatement();
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, 1); // set the value of the first parameter to 123
+			ResultSet rs = pstmt.executeQuery();
+			//ResultSet rs = stmt.executeQuery("SELECT * FROM mytable");
 			while (rs.next()) {
 				int id = rs.getInt("id");
 				String name = rs.getString("name");
 				System.out.println("ID: " + id + ", Name: " + name);
 			}
-			rs.close();
-			stmt.close();
+			//stmt.close();
 			conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
